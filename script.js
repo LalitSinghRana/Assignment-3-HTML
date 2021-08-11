fde1b6aaa9883f6120cc30f700fa147e44ce226f
Lalit Rana <lalit.rana@nagarro.com>
2021-08-11 13:46:26 +0530

    Completed

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
-// function logSubmit(event) {
-// 	log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
-
-// 	let name = form.elements['name'].value;
-// 	let email = form.elements['email'].value;
-// 	let website = form.elements['website'].value;
-// 	let image = form.elements['image'].value;
-// 	let gender = form.elements['gender'].value;
-// 	log.textContent = ` ${name}, ${email}, ${website}, ${image}, ${gender}`;
-
-// 	var div = document.createElement('div');
-// 	div.style.backgroundColor = 'black';
-// 	// div.style.position = 'absolute';
-// 	// div.style.left = '50px';
-// 	// div.style.top = '50px';
-// 	div.style.height = '10px';
-// 	div.style.width = '10px';
-
-// 	event.preventDefault();
-// }
-
-// const form = document.getElementById('form');
-// const log = document.getElementById('log');
-// form.addEventListener('submit', logSubmit);
-
 let myBool = true;
 
 function myFunction() {
+	event.preventDefault();
+
 	if (validate()) {
 		let name = form.elements['name'].value;
 		let email = form.elements['email'].value;
@@ -44,7 +21,7 @@ function myFunction() {
 
 		let div1 = document.createElement('div');
 		div1.className = 'card-left';
-		div1.innerHTML = `${name} <br/> 
+		div1.innerHTML = `<strong>${name}</strong> <br/> 
 											${gender} <br/> 
 											${email} <br/> 
 											<a href="${website}" target="_blank">${website.split('/')[2]}</a> <br/> 
@@ -67,10 +44,9 @@ function myFunction() {
 		document.getElementById('card-dis').appendChild(div1);
 		document.getElementById('card-img').appendChild(div2);
 	}
-
-	event.preventDefault();
 }
 
+// Validating checkbox group
 function validate() {
 	let form_data = new FormData(document.querySelector('form'));
 
